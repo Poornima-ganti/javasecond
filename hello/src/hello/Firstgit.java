4dfdb369df13f63ef80831d91dd9fbdf458fa684
praveenkumarbejjarapu <praveenkumar6128@gmail.com>
2020-10-22 19:45:02 +0530

    code written

---
+++ hello/src/hello/Firstgit.java
@@ -0,0 +1,23 @@
+package hello;
+
+public class Firstgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+			// TODO Auto-generated method stub
+	String s="hi i am new to java  and just leraning";
+	int count=0;
+	for(int i=0;i<s.length();i++)
+	{
+		if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u')
+		{
+	count++;
+
+		  }
+		}
+		System.out.println("vowels count is " +count);
+
+		
+	}
+
+}
